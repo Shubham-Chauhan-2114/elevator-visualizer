3814cd498be5b2cff75758868769acab79ebf61c
Shubham-Chauhan-2114 <shubhamchauhancode@gmail.com>
2025-07-24 16:55:03 +0530

    Done finally

--- elevator-backend/testcase_writer.py
+++
@@ -1,3 +0,0 @@
-def write_testcase(filename, elevators, floors, solvers, max_turns,max_requests):
-    with open(filename, "w") as f:
-        f.write(f"{elevators} {floors} {solvers} {max_turns} {max_requests}\n")# 0 = initial #requests
